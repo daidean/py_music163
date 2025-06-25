b1d52a2eff0316a20b1992588cd1ea6325c75b29
dean <ddiq@msn.com>
2025-06-26 02:32:11 +0800

    1.0

--- main.py
+++ main.py
@@ -1,5 +1,70 @@
+import logging
+import requests
+
+import config
+import signer
+
+session = requests.session()
+tasks = {}
+
+
+logger_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logger_formatter = logging.Formatter(logger_format)
+logger_handler_console = logging.StreamHandler()
+logger_handler_console.setFormatter(logger_formatter)
+logger_handler_file = logging.FileHandler(f"{__file__}.log")
+logger_handler_file.setFormatter(logger_formatter)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logger_handler_console)
+logger.addHandler(logger_handler_file)
+
+
+def music163_login():
+    session.cookies.set("MUSIC_U", config.Cookie_MUSIC_U)
+    session.cookies.set("__csrf", config.Cookie_CSRF)
+
+    resp = session.get(config.url_UserInfo)
+    assert resp.status_code == 200
+
+    resp = resp.json()
+    assert resp["code"] == 200
+
+    username = resp["profile"]["nickname"]
+    logger.info(f"登录成功，用户名：{username}")
+
+
+def music163_fetch_tasks():
+    global tasks
+
+    resp = session.get(config.url_Tasks)
+    assert resp.status_code == 200
+
+    resp = resp.json()
+    assert resp["code"] == 200
+
+    tasks = resp["data"]
+    tasks_total = tasks["count"]
+    tasks_done = tasks["completedCount"]
+    logger.info(f"任务获取成功，总量：{tasks_total}，已完成：{tasks_done}")
+
+
+def music163_complete_tasks():
+    sig = signer.Signer(session, tasks["id"], logger)
+
+    for work in tasks["works"]:
+        if work["completed"]:
+            continue
+        sig.sign(work["work"])
+
+
 def main():
-    print("Hello from py-music163!")
+    logger.info("Hello from py-music163!")
+    # source: https://github.com/ACAne0320/ncmp
+
+    music163_login()
+    music163_fetch_tasks()
+    music163_complete_tasks()
 
 
 if __name__ == "__main__":
